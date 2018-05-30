623f909dc3a983100b5f8789f8ca342a80fa9dfe
misaellandero <misaellandero@landercorp.com.mx>
2018-05-30 14:07:01 -0500

    Ahora se muestran la imagen del clima y una texto

--- app/src/main/java/com/example/misael_mac/appclima/MainActivity.java
+++ app/src/main/java/com/example/misael_mac/appclima/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.misael_mac.appclima;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView iv_weather = (ImageView)findViewById(R.id.iv_weather);
+        int identificador = getResources().getIdentifier("imagen_01n","drawable", getPackageName());
+        iv_weather .setImageDrawable(getResources().getDrawable(identificador,null));
+
     }
 }
